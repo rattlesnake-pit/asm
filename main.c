f3b81b9ffae428a0629e37d9ade42ea3545abd61
Antonio de Luna <tonydelun@gmail.com>
2016-09-18 20:24:38 -0700

    fix a bug when printing byte 0A on windows

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include "stdlib.h"
 #include "string.h"
 
+#ifdef _WIN32
+#include <io.h>
+#include <fcntl.h>
+#endif
+
 int PC = 0;
 int DS = 0;
 int st_end = 0;
@@ -411,6 +416,9 @@ void resolvePendingLabels() {
 }
 
 int main() {
+#ifdef _WIN32
+  _setmode( _fileno(stdout), _O_BINARY);
+#endif
   init();
   assembler();
   resolvePendingLabels();
